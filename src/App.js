d9f1dbfd1b92cd5f2a99ff264ca43cd458bc729d
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-09 22:10:25 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project is coded by Chisenga Chisulo and is{" "}
-        <a
-          href="https://github.com/chisengachisulo/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project is coded by Chisenga Chisulo and is{" "}
+          <a
+            href="https://github.com/chisengachisulo/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
