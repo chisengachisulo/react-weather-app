afc88ac2307f66a5832717edb472e52824ff4bf9
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-09 21:23:59 +0000

    added Github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is coded by Chisenga Chisulo and is{" "}
+        <a
+          href="https://github.com/chisengachisulo/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
